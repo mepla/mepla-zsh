3878c1ead651bb150a4fedd1673f04ff2e5c1e0c
mepla <soheil.nasirian@gmail.com>
2022-02-07 10:54:58 +0100

    Add flutter data model creation

--- mepla-zsh.plugin.zsh
+++ mepla-zsh.plugin.zsh
@@ -27,14 +27,18 @@ alias grv='git remote -v'
 alias gs='git status -s'
 
 export function gap() {
-  gs || exit 1
+  gs || return 1
   read "cont?Are you sure? (y/N): " 
   [[ "$cont" == "y" ]] && echo && ga . && gca && gpuf
 }
 
 export function gcp() {
-  gs || exit 1
+  gs || return 1
+
+  echo  
   echo "Commit message: $1"
+  echo
+
   read "cont?Are you sure? (y/N): " 
   if [[ $# == 1 ]]
     then
@@ -47,3 +51,24 @@ export function gcp() {
 ######################## FLUTTER #######################
 
 alias fbr='flutter pub run build_runner build --delete-conflicting-outputs'
+
+
+export function fcm() {
+class_body='
+import 'package:json_annotation/json_annotation.dart';
+part '_FILENAME_.g.dart';
+
+@JsonSerializable()
+class _CLASSNAME_ {
+  final String id;
+  final String name;
+
+  _CLASSNAME_({this.id = "", this.name = ""});
+
+  factory _CLASSNAME_.fromJson(Map<String, dynamic> json) => _$_CLASSNAME_FromJson(json);
+  Map<String, dynamic> toJson() => _$_CLASSNAME_ToJson(this);
+}
+'
+
+echo "$class_body" | sed -e "s/_FILENAME_/$1/g" | sed -e "s/_CLASSNAME_/$2/g" 
+}
